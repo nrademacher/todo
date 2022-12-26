a2fabc8c60ef7d90e3b2a5a7e49b2bb015894af3
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-12-26 01:36:51 +0100

    feat(server): add server-side input validation

--- apps/server/src/routers/todo-router.ts
+++ apps/server/src/routers/todo-router.ts
@@ -1,5 +1,7 @@
 import { type Request, type Response, Router } from "express";
+import { body } from "express-validator";
 import { TodoController } from "../controllers";
+import { validate } from "../middlewares";
 
 const todoRouter = Router();
 
@@ -11,13 +13,29 @@ todoRouter.get("/:id", async (req: Request, res: Response) => {
   await TodoController.getTodoById(Number(req.params.id), res);
 });
 
-todoRouter.post("/", async (req: Request, res: Response) => {
-  await TodoController.createTodo(req.body, req.user.id, res);
-});
+todoRouter.post(
+  "/",
+  [
+    body("done").toBoolean(),
+    body("description").isString(),
+    validate,
+  ],
+  async (req: Request, res: Response) => {
+    await TodoController.createTodo(req.body, req.user.id, res);
+  },
+);
 
-todoRouter.patch("/:id", async (req: Request, res: Response) => {
-  await TodoController.updateTodo(Number(req.params.id), req.body, res);
-});
+todoRouter.patch(
+  "/:id",
+  [
+    body("done").toBoolean().optional(),
+    body("description").isString().optional(),
+    validate,
+  ],
+  async (req: Request, res: Response) => {
+    await TodoController.updateTodo(Number(req.params.id), req.body, res);
+  },
+);
 
 todoRouter.delete("/:id", async (req: Request, res: Response) => {
   await TodoController.deleteTodo(Number(req.params.id), res);
