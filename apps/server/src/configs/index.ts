c82e0592ce1bc2a44fa5f793fce08b52493c4c9a
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-12-26 14:44:50 +0100

    feat(server): add env configs

---
+++ apps/server/src/configs/index.ts
@@ -0,0 +1,46 @@
+import merge from "lodash.merge";
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const stage = process.env.STAGE || "local";
+
+type Config = {
+  stage: string;
+  env: string;
+  port: number;
+  secrets: {
+    jwt: string;
+    db: {
+      host: string;
+      name: string;
+      password: string;
+    };
+  };
+};
+
+export type EnvConfig = Partial<Config>
+
+const baseConfig: Config = {
+  stage,
+  env: process.env.NODE_ENV,
+  port: Number(process.env.PORT),
+  secrets: {
+    jwt: process.env.JWT_SECRET,
+    db: {
+      host: process.env.DB_HOST,
+      name: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+    },
+  },
+};
+
+let envConfig: Config;
+if (stage === "production") {
+  envConfig = require("./prod").default;
+} else if (stage === "testing") {
+  envConfig = require("./testing").default;
+} else {
+  envConfig = require("./local").default;
+}
+
+export const config = merge(baseConfig, envConfig);
