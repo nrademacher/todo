56cbcaa456fffa08df01c8f61dfed3471392255b
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2023-01-07 17:29:06 +0100

    fix(server): lint error

--- apps/server/src/database/data-source.ts
+++ apps/server/src/database/data-source.ts
@@ -1,5 +1,6 @@
 import { DataSource, type DataSourceOptions } from "typeorm";
 import { config } from "../configs";
+import { join } from "path";
 
 const dataSourceOptions: DataSourceOptions = {
   type: "mysql",
@@ -8,7 +9,7 @@ const dataSourceOptions: DataSourceOptions = {
   username: config.secrets.db.username,
   password: config.secrets.db.password,
   database: config.secrets.db.name,
-  entities: [`${__dirname}/entities/*.entity{.js,.ts}`],
+  entities: [join(__dirname, "entities/*.entity{.js,.ts}")],
   migrations: [],
   logging: true,
   synchronize: config.env !== "production",
