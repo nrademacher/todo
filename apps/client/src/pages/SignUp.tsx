e09c6a6facba810257dc57d4d96b05878c26de09
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2023-01-15 14:38:56 +0100

    feat: add prerelease disclaimer to sign-up page

--- apps/client/src/pages/SignUp.tsx
+++ apps/client/src/pages/SignUp.tsx
@@ -3,10 +3,22 @@ import { useAuth } from "../hooks";
 import type { CreateUserParams } from "../api";
 import type { AxiosError } from "axios";
 import { Link, Navigate } from "react-router-dom";
+
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Unstable_Grid2";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+
+const PrereleaseDisclaimer: React.FC = () => (
+  <Alert severity="warning">
+    <AlertTitle>Disclaimer</AlertTitle>
+    This is an early pre-release version of Todo. Fundamental changes and loss
+    of user data may occur at any time without notice.
+  </Alert>
+);
 
 export function SignUp(): JSX.Element {
   const [username, setUsername] = useState("");
@@ -48,64 +60,69 @@ export function SignUp(): JSX.Element {
   return (
     <Container>
       <h1>Sign Up</h1>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          await handleSignUp();
-        }}
-      >
-        <Grid container spacing={2} direction="column">
-          <Grid xs={12} sm={8} md={4}>
-            <TextField
-              label="Username"
-              id="username-input"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              fullWidth
-            />
+      <Stack sx={{ width: "100%" }} spacing={2}>
+        <PrereleaseDisclaimer />
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            await handleSignUp();
+          }}
+        >
+          <Grid container spacing={2} direction="column">
+            <Grid xs={12} sm={8} md={4}>
+              <TextField
+                label="Username"
+                id="username-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                fullWidth
+              />
+            </Grid>
+            <Grid xs={12} sm={8} md={4}>
+              <TextField
+                label="Email"
+                id="email-input"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                fullWidth
+              />
+            </Grid>
+            <Grid xs={12} sm={8} md={4}>
+              <TextField
+                label="Password"
+                id="password-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                fullWidth
+              />
+            </Grid>
+            <Grid xs={12} sm={8} md={4}>
+              <Button
+                variant="contained"
+                type="submit"
+                disabled={username === "" || email === "" || password === ""}
+              >
+                Sign up
+              </Button>
+            </Grid>
+            <Grid xs={12} sm={8} md={4}>
+              <Link to="/auth/signin">
+                <Button>Sign in</Button>
+              </Link>
+            </Grid>
           </Grid>
-          <Grid xs={12} sm={8} md={4}>
-            <TextField
-              label="Email"
-              id="email-input"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              fullWidth
-            />
-          </Grid>
-          <Grid xs={12} sm={8} md={4}>
-            <TextField
-              label="Password"
-              id="password-input"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              fullWidth
-            />
-          </Grid>
-          <Grid xs={12} sm={8} md={4}>
-            <Button
-              variant="contained"
-              type="submit"
-              disabled={username === "" || email === "" || password === ""}
-            >
-              Sign up
-            </Button>
-          </Grid>
-          <Grid xs={12} sm={8} md={4}>
-            <Link to="/auth/signin">
-              <Button>Sign in</Button>
-            </Link>
-          </Grid>
-        </Grid>
-      </form>
-      {errors.signUp !== null ? (
-        <p style={{ color: "red" }}>
-          {handleSubmitError((errors.signUp as AxiosError).response?.status)}
-        </p>
-      ) : null}
+        </form>
+      </Stack>
+      {errors.signUp !== null
+        ? (
+          <p style={{ color: "red" }}>
+            {handleSubmitError((errors.signUp as AxiosError).response?.status)}
+          </p>
+        )
+        : null}
     </Container>
   );
 }
