67aace0e27b12ef0011844dbafe558d3d2c074cd
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2023-01-21 23:52:38 +0100

    fix: fix bad migration

---
+++ apps/server/src/database/migrations/1674341353000-add_todo_title.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class addTodoTitle1674341353000 implements MigrationInterface {
+    name = 'addTodoTitle1674341353000'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`todo\` ADD \`title\` varchar(255) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`todo\` CHANGE \`description\` \`description\` varchar(255) NOT NULL DEFAULT ''`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`todo\` CHANGE \`description\` \`description\` varchar(255) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`todo\` DROP COLUMN \`title\``);
+    }
+
+}
